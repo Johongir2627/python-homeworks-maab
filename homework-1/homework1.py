732f607611bfa485efb1f96a18a65eb95d972957
Johongir2627 <johongirdagarov@gmail.com>
2025-03-12 17:49:51 +0500

    Create homework1.py

---
+++ homework-1/homework1.py
@@ -0,0 +1,30 @@
+#Homework-1
+a = int(input('Enter a side of square: '))
+b = 4 * a
+print(f"Perimeter of the square is: {b}")
+
+
+#Homework-2
+c = int(input('Enter diameter of circle: '))
+d = 3.14 * c
+print(f"Length of the cicrle is: {d}")
+
+
+#Homework-3
+first_num = int(input('Enter first number, a = '))
+second_num = int(input('Enter second number, b = '))
+mean = (first_num + second_num) / 2
+print(f'Mean of a and b is {mean}')
+
+
+#Homework-4
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(f"Sum: {sum_ab}")
+print(f"Product: {product_ab}")
+print(f"Square of {a}: {square_a}")
+print(f"Square of {b}: {square_b}")
